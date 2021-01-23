cea59b581ea9865fb7709bb8a8e870076e4807ef
t-simeon-gordon <terrel.simeon-gordon@accenture.com>
2021-01-23 08:50:16 +0000

    v0 add dice game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDice = 1;
+  var rightDice = 1;
+
+  void rollDice(){
+    leftDice = Random().nextInt(6)+1;
+    rightDice = Random().nextInt(6)+1;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(child:
+    Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  rollDice();
+                });
+              },
+              child: Image.asset('images/dice$leftDice.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                    rollDice();
+                  });
+                },
+              child: Image.asset('images/dice$rightDice.png'),
+            ),
+          ),
+        ],
+      ),
+    ),
+    );
   }
 }
